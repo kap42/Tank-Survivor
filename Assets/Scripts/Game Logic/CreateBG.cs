76fd188a6b272dd3e3481a72e162203f225cb7de
Patrik Grip <pgj@gnulix.org>
2022-02-16 22:29:14 +0100

    Added basic background

---
+++ Assets/Scripts/Game Logic/CreateBG.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CreateBG : MonoBehaviour
+{
+    public List<TileBase> BackgroundTiles = new List<TileBase>();
+
+    public int startX = -10;
+    public int startY = -10;
+
+    public int w = 20;
+    public int h = 20;
+
+    Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = GameObject.Find("Tilemap").GetComponent<Tilemap>();
+
+        Vector3Int pos = Vector3Int.zero;
+
+        for (int y = startY; y < startY+h; y++)
+        {
+            for (int x = startX; x < startX + w; x++)
+            {
+                pos.x = x;
+                pos.y = y;
+
+                tilemap.SetTile(
+                    pos,
+                    BackgroundTiles[
+                        Random.Range(0,BackgroundTiles.Count)
+                    ]);
+            }
+        }
+    }
+}
